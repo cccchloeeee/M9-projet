9e70c09e7d1fb00d67474fbcdeaa6bcd2f5dd806
cccchloeeee <99724816+cccchloeeee@users.noreply.github.com>
2022-02-15 11:15:36 +0100

    Add files via upload

---
+++ test0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 10:50:57 2022
+
+@author: chloechallamel
+"""
+
+a = float(input('a = '))
+b = float(input('b = '))
+print(a + b)
